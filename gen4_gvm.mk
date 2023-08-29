829dd4c6ea6e0c2b565e90925d2d71179ede5996
Swapneel Mashalkar <quic_smashalk@quicinc.com>
2023-08-29 11:18:07 +0530

    [gen4_gvm]: Set SHIPPING_API=34
    Setting PRODUCT_SHIPPING_API_LEVEL = 34
    
    Signed-off-by: Swapneel Mashalkar <quic_smashalk@quicinc.com>
    Change-Id: I7daa96ba6253070a5f80a053dfd5406789ac2de6

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -231,7 +231,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml
 
 
-PRODUCT_SHIPPING_API_LEVEL := 33
+PRODUCT_SHIPPING_API_LEVEL := 34
 
 #Initial bringup flags
 
