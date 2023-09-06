1d6c84940dfcec386ba4f3e437988df85efb17b5
Asmeer Mohammed <quic_asmemoha@quicinc.com>
2023-09-06 14:40:22 +0530

    gen4_gvm: Update HALs for API Level 34
    - remove android.hardware.media.omx (deprecated)
    - replace thermal hidl hal with aidl hal
    - cleanup legacy fstab
    - manifest cleanup
        -> removed unused android.hardware.graphics.mapper 2.1 hidl
        declaration
        -> android.hardware.sensors hidl entry removed
        -> removed vendor.qti.hardware.bluetooth_sar - unused for auto
    - Added default BroadcastRadio Hal
    
    Change-Id: I34159a0a335db50a95448c0072f280bc86411bd7
    Signed-off-by: Asmeer Mohammed <quic_asmemoha@quicinc.com>

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -23,14 +23,14 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.soc.model=$(PRODUCT_DEVICE)
 
 ALLOW_MISSING_DEPENDENCIES := true
-  ENABLE_AB ?= true
-  # Enable virtual-ab by default
-  ifeq ($(ENABLE_AB), true)
-    ENABLE_VIRTUAL_AB ?= false
-  endif
-  ifeq ($(ENABLE_VIRTUAL_AB), true)
-    $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-  endif
+ENABLE_AB ?= true
+# Disable virtual-ab by default
+ifeq ($(ENABLE_AB), true)
+  ENABLE_VIRTUAL_AB ?= false
+endif
+ifeq ($(ENABLE_VIRTUAL_AB), true)
+  $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+endif
 # Enable AVB 2.0
 BOARD_AVB_ENABLE := true
 BOARD_USES_QCNE := false
@@ -43,8 +43,6 @@ TARGET_USES_QTIC := false
 TARGET_USES_QTIC_EXTENSION := false
 ENABLE_HYP := true
 TARGET_CONSOLE_ENABLED ?= true
-# FR77687: Migrate AIDL interface using -ndk_platform.so to -ndk.so
-NEED_AIDL_NDK_PLATFORM_BACKEND := true
 TARGET_NO_QTI_WFD := true
 BOARD_HAVE_QCOM_FM := false
 BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := false
@@ -73,7 +71,6 @@ TARGET_HAS_VIRTIO_FASTRPC := true
 # Dynamic-partition enabled by default
 BOARD_DYNAMIC_PARTITION_ENABLE := true
 ifeq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
-
   PRODUCT_USE_DYNAMIC_PARTITIONS := true
   BOARD_BUILD_SUPER_IMAGE_BY_DEFAULT := true
   PRODUCT_BUILD_SUPER_PARTITION := true
@@ -81,18 +78,16 @@ ifeq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
   # Enable System_ext
   PRODUCT_BUILD_SYSTEM_EXT_IMAGE := true
   PRODUCT_PACKAGES += fastbootd
-  
-# Mismatch in the uses-library tags between build system and the manifest leads
-# to soong APK manifest_check tool errors. Enable the flag to fix this.
-RELAX_USES_LIBRARY_CHECK := true
 
-ifeq ($(ENABLE_AB), true)
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_AB_dynamic_partition_variant.gen4.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.gen4.qcom
-else
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB_dynamic_partition_variant.gen4.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.gen4.qcom
-endif
+  # Mismatch in the uses-library tags between build system and the manifest leads
+  # to soong APK manifest_check tool errors. Enable the flag to fix this.
+  RELAX_USES_LIBRARY_CHECK := true
+  
+  ifeq ($(ENABLE_AB), true)
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_AB_dynamic_partition_variant.gen4.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.gen4.qcom
+  else
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB_dynamic_partition_variant.gen4.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.gen4.qcom
+  endif
 endif
 #PRODUCT_BUILD_SYSTEM_IMAGE := true
 PRODUCT_BUILD_SYSTEM_OTHER_IMAGE := false
@@ -116,7 +111,7 @@ BOARD_AVB_SYSTEM_DLKM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 BOARD_AVB_VENDOR_DLKM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 
 ifneq ("$(wildcard device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/vendor_dlkm/system_dlkm.modules.blocklist)", "")
-PRODUCT_COPY_FILES += device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/vendor_dlkm/system_dlkm.modules.blocklist:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
+  PRODUCT_COPY_FILES += device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/vendor_dlkm/system_dlkm.modules.blocklist:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
 endif
 
 TARGET_DEFINES_DALVIK_HEAP := true
@@ -241,7 +236,7 @@ PRODUCT_SHIPPING_API_LEVEL := 34
 
 #Default vendor image configuration
 ifeq ($(ENABLE_VENDOR_IMAGE),)
-ENABLE_VENDOR_IMAGE := false
+  ENABLE_VENDOR_IMAGE := false
 endif
 
 TARGET_KERNEL_VERSION := 6.1
@@ -263,12 +258,12 @@ PRODUCT_PACKAGES += libGLES_android
 
 # diag-router
 ifeq ($(strip $(TARGET_BUILD_VARIANT)),user)
-    TARGET_HAS_DIAG_ROUTER := false
+  TARGET_HAS_DIAG_ROUTER := false
 else
-    TARGET_HAS_DIAG_ROUTER := true
+  TARGET_HAS_DIAG_ROUTER := true
 endif
 
-# Memtrack HAL deprecated. Replaced with AIDL for target-level 6.
+# Memtrack HAL deprecated. Replaced with AIDL for target-level >= 6.
 ENABLE_MEMTRACK_AIDL_HAL := true
 
 -include $(QCPATH)/common/config/qtic-config.mk
@@ -308,8 +303,6 @@ PRODUCT_COPY_FILES += device/qcom/gen4/media_codecs_performance.xml:$(TARGET_COP
 endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 
 #PRODUCT_COPY_FILES += hardware/qcom/media/conf_files/gen4/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
-
-PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 #Audio DLKM
 AUDIO_DLKM := audio_apr.ko
@@ -376,9 +369,12 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/
 # Enable Scoped Storage related
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Display/Graphics
+# BroadcastRadio
 PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl
+    android.hardware.broadcastradio-service.default
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
 
 # MSM IRQ Balancer configuration file
 #PRODUCT_COPY_FILES += device/qcom/gen4/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
@@ -436,7 +432,7 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.usb.diag_mdm.inst.name=diag_mdm2
 
 #Copy supported features list
 ifeq ($(TARGET_USES_GAS),true)
-PRODUCT_COPY_FILES += device/qcom/gen4_gvm/gen4_gvm_features.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/gen4_gvm_features.xml
+  PRODUCT_COPY_FILES += device/qcom/gen4_gvm/gen4_gvm_features.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/gen4_gvm_features.xml
 endif
 
 # Camera configuration file. Shared by passthrough/binderized camera HAL
@@ -464,7 +460,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += android.hardware.health-service.example \
                     android.hardware.dumpstate-service.example \
-                    android.hardware.thermal@2.0-service.mock
+                    android.hardware.thermal-service.example
 
 PRODUCT_PACKAGES += android.hardware.gnss@2.0-service
 PRODUCT_PACKAGES += qcar-gsi.avbpubkey
