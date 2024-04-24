96f87575160ece1434581cc715bf9d08bbf503c4
Vijayakumar Badiger <quic_vbadiger@quicinc.com>
2024-04-24 15:30:21 -0700

    gen4_gvm: fix compilation for derived target
    Adding of boot, init_boot and vendor_boot is causing compilation
    issue for gen4_gvm_gy derived target as these images are not enabled.
    For gen4_gvm_gy guard the addition of the new images using the OS
    specific flag.
    
    Signed-off-by: Vijayakumar Badiger <quic_vbadiger@quicinc.com>
    Change-Id: Idc6cd61be013e74a8e700662e879b30542d799e2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,15 +124,20 @@ endif
 AB_OTA_UPDATER := true
 ifeq ($(ENABLE_AB), true)
  # Full A/B partition update set
-  ifeq ($(TARGET_SINGLE_TREE), true)
-    AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm system system_ext product boot init_boot vendor_boot
-  else ifeq ($(TARGET_BOARD_DERIVATIVE_SUFFIX), _microdroid)
-    AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm system system_ext product boot init_boot vendor_boot
-  else  ifeq ($(strip $(TARGET_BOARD_DERIVATIVE_SUFFIX)),_gy)
-    AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm
+  ifneq ($(TARGET_USES_GY), true)
+    ifeq ($(TARGET_SINGLE_TREE), true)
+      AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm system system_ext product boot init_boot vendor_boot
+    else ifeq ($(TARGET_BOARD_DERIVATIVE_SUFFIX), _microdroid)
+      AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm system system_ext product boot init_boot vendor_boot
+    else
+      AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm boot init_boot vendor_boot
+    endif
   else
-    AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm boot init_boot vendor_boot
-  endif
+      AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm
+     ifeq ($(TARGET_SINGLE_TREE), true)
+      AB_OTA_PARTITIONS ?= vendor vbmeta vendor_dlkm system_dlkm system system_ext product
+     endif
+  endif #TARGET_USES_GY
 else
   AB_OTA_PARTITIONS ?= boot system
   ifneq ($(BOARD_USES_RECOVERY_AS_BOOT), true)
