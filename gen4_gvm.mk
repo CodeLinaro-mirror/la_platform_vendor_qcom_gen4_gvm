e9be57d584195db4b3ef7b47edc7f339ec712204
katamoni sai abhilash <quic_katamoni@quicinc.com>
2024-06-20 18:05:14 +0530

    gen4_gvm: drm: Invoke script to set persist.vendor.drm.smmu property
    Add persist.vendor.drm.smmu property and Invoke
    init.drm.smmu.sh for setting the property for Automotive
    Gen4 chipsets and set DRM_SMMU flag to true as smmu property will be
    initialized with this change.
    
    Change-Id: Ia17244cebae154eab9643efa6bcc5f3b6cf6223e
    Signed-off-by: katamoni sai abhilash <quic_katamoni@quicinc.com>

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -266,7 +266,7 @@ TARGET_USES_QMAA_OVERRIDE_DIAG := false
 TARGET_USES_QMAA_OVERRIDE_DISPLAY := true
 TARGET_USES_QMAA_OVERRIDE_DPM  := false
 TARGET_USES_QMAA_OVERRIDE_DRM  := true
-TARGET_USES_QMAA_OVERRIDE_DRM_SMMU  := false
+TARGET_USES_QMAA_OVERRIDE_DRM_SMMU  := true
 TARGET_USES_QMAA_OVERRIDE_EID := false
 TARGET_USES_QMAA_OVERRIDE_FASTCV  := true
 TARGET_USES_QMAA_OVERRIDE_FASTRPC := false
@@ -414,6 +414,10 @@ PRODUCT_PACKAGES += android.hardware.boot@1.0-impl \
                     android.hardware.boot@1.0-service \
                     update_engine_sideload
 endif
+
+# drm_smmu property
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.drm.smmu=0
 
 # bootctrl property
 PRODUCT_VENDOR_PROPERTIES += \
