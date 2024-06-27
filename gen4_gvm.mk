abb43f335e57a5c7f6db14ddc3cc625f30d60bea
Tabassum Tabassum <quic_ttabassu@quicinc.com>
2024-06-27 18:34:10 +0530

    msmnile_gvmq : Including generic_ramdisk.mk to pass VTS test
    Including generic_ramdisk.mk to pass below VTS test
    case which required snapuserd and snapuserd.rc in
    ramdisk.
    VTS test case : vts_generic_boot_image_test
    
    Signed-off-by: Tabassum Tabassum <quic_ttabassu@quicinc.com>
    Change-Id: I0cdc7e503b225557d6e33bf871159afa4fc9da7b

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -70,6 +70,7 @@ ifeq ($(ENABLE_VIRTUAL_AB), true)
   endif
   ifeq (true,$(call math_gt_or_eq,$(SHIPPING_API_LEVEL),34))
     # For OTA updates with shipping api level 34 and above.
+    $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
     $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
     PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
   else
