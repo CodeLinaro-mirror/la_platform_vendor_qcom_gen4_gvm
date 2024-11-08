3b3323e4a6d489026a56321022b41d75e1209e0e
Sumit Chakraborty <quic_sumichak@quicinc.com>
2024-11-08 16:21:53 +0530

    [Android W]: Add Android W support to enable compilation with 25Q1
    Change-Id: Iff2ea92a42fd7d25d39cc74bb720e8d188e7fcb5
    Signed-off-by: Sumit Chakraborty <quic_sumichak@quicinc.com>
    (cherry picked from commit 71839476da73f9ab62de37f22def8202a50499d9)

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -24,7 +24,7 @@ ifeq ($(TARGET_SINGLE_TREE), true)
   PRODUCT_PRODUCT_VNDK_VERSION := current
   #TODO(amutyala) to revert once QSSI 15 component created
   #This change requires to build super image (QSSI15 + V14)
-  ifeq (,$(filter VanillaIceCream V 35, $(PLATFORM_VNDK_VERSION)))
+  ifeq (,$(filter VanillaIceCream V 35 W Baklava 16, $(PLATFORM_VNDK_VERSION)))
     PRODUCT_EXTRA_VNDK_VERSIONS := 33
   else
     PRODUCT_EXTRA_VNDK_VERSIONS := 33 34
@@ -89,7 +89,7 @@ BOARD_USES_QCNE := false
 TARGET_BOARD_AUTO := true
 TARGET_USES_AOSP := true
 #TODO(amutyala) to revert this once QSSI 15 component created
-ifeq (,$(filter VanillaIceCream V 35, $(PLATFORM_VNDK_VERSION)))
+ifeq (,$(filter VanillaIceCream V 35 W Baklava 16, $(PLATFORM_VNDK_VERSION)))
   TARGET_USES_GAS := true
 endif
 TARGET_USES_QCOM_BSP := false
