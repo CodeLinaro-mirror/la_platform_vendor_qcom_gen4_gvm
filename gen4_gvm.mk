edafa3729cefa77aa65be03e9d264792629d41ae
Ian Wei <quic_ianwei@quicinc.com>
2024-12-02 22:41:59 +0800

    gen4_gvm: variable add for eavb fe in the gen4 gvm
    eavb tinyalsa plugin
    
    Change-Id: I90a16cb0d3eeec0cb09dc1dd3905141f0bb78355
    Signed-off-by: Ian wei <quic_ianwei@quicinc.com>
    (cherry picked from commit 65a0c097314b59470e2d910921ebca5b312e5e01)

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -553,7 +553,9 @@ PRODUCT_PACKAGES += \
 
 #eavb fe lib and app
 PRODUCT_PACKAGES += libeavbfe \
-            eavbfe_test
+            eavbfe_test \
+            libqavb_fe_pcm_plugin \
+            tinyalsa_eavbfe
 
 #Boot control HAL test app
 PRODUCT_PACKAGES_DEBUG += bootctl
