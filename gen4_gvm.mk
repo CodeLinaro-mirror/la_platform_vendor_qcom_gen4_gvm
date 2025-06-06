80ccf81a4539c2ec79f70779303335c96fffbb84
Tabassum Tabassum <quic_ttabassu@quicinc.com>
2025-06-06 10:38:39 +0530

    gen4_gvm: Disable Broadcast radio HAL
    Since there is no use cases related to
    Broadcast radio HAL on Automotive as of
    now therefore disabling this HAL.
    VTS testcases:VtsHalBroadcastradioAidlTargetTest
    
    Change-Id: I4b7bc3e45dfe3b76de950b7496c40794b678408a
    Signed-off-by: Tabassum Tabassum <quic_ttabassu@quicinc.com>

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -463,10 +463,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/
 # Enable Scoped Storage related
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# BroadcastRadio
-PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio-service.default
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
 
