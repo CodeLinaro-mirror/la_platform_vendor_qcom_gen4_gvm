b902d29d9d79ff202c240b7e4097cc89a422ecf9
vidyalak <quic_vidyalak@quicinc.com>
2025-07-26 00:18:40 +0530

    [gen4_gvm] Enabling health Hal for xTS fix
    Change-Id: I77664bd05b67f17664d52970d1adc17d0385de5b
    Signed-off-by: vidyalak<quic_vidyalak@quicinc.com>

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -583,7 +583,8 @@ PRODUCT_PACKAGES += \
 
 #PRODUCT_PACKAGES += android.hardware.automotive.audiocontrol@1.0-service
 
-PRODUCT_PACKAGES += android.hardware.health-service.example \
+PRODUCT_PACKAGES += android.hardware.health-service.qti \
+                    android.hardware.health-service.qti_recovery \
                     android.hardware.dumpstate-service.example \
                     android.hardware.thermal-service.example
 
