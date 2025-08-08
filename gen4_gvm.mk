12cd4c5a7f19a3174aef65d94e5795ab90d4c749
Tabassum Tabassum <quic_ttabassu@quicinc.com>
2025-08-08 14:34:00 +0530

    gen4_gvm: Disbaled google health HAL and updating userdata
    1. Disbaled google health HAL, and enabled health HAL.
    2. Added "nosuid,nodev" flags for userdata partition.
    
    Change-Id: I565afd81c318cd1ace372d53a7d7c88102b146b2
    Signed-off-by: Tabassum Tabassum <quic_ttabassu@quicinc.com>

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -582,9 +582,7 @@ PRODUCT_PACKAGES += \
 
 #PRODUCT_PACKAGES += android.hardware.automotive.audiocontrol@1.0-service
 
-PRODUCT_PACKAGES += android.hardware.health-service.qti \
-                    android.hardware.health-service.qti_recovery \
-                    android.hardware.dumpstate-service.example \
+PRODUCT_PACKAGES += android.hardware.dumpstate-service.example \
                     android.hardware.thermal-service.example \
                     com.android.hardware.thermal
 
