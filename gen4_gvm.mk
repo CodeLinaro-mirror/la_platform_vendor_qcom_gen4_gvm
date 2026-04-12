7cc480d3f7cc374f19a595605077b36f6baca430
pengweic <pengweic@qti.qualcomm.com>
2026-04-12 15:33:24 +0530

    gen4_gvm: Added LPM support for gptp
    Change-Id: Iab7890feaaf667800cbd2c14e5b7b9e8556dcc68
    Signed-off-by: pengweic <pengweic@qti.qualcomm.com>

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -433,7 +433,8 @@ PRODUCT_PACKAGES += qgptp \
     libgptp_test \
     utc_ts \
     libutc.so \
-    libutc_test
+    libutc_test \
+    qgptp_powerservice
 
 
 PRODUCT_PACKAGES += fs_config_files
