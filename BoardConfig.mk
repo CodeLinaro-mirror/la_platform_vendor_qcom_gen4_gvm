19ed58bda83d80d9030c54c2054035146aefb7d9
Gourab Dalal <gdalal@qti.qualcomm.com>
2026-08-14 21:24:25 +0530

    Added Soong Variable to Enable/Disable Display Safety
    Changes.
    Added variable and soong variable to enable/disable display safety.
    If it is set to true then display safety shall be part of build otherwise not.
    
    Change-Id: Ib7d946e1c7fcf9cda1df08854c69bc6f104a45a3
    Signed-off-by: Gourab Dalal <gdalal@qti.qualcomm.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -310,6 +310,12 @@ AOSAL_BUILD_WITH_SOONG := false
 $(call soong_config_namespace, qti_aosal)
 $(call soong_config_set_bool, qti_aosal, AOSAL_BUILD_SOONG, false)
 #----------------------------------------------------------------------
+#----------------------------------------------------------------------
+#Soong config for Display Safety
+TARGET_DISPLAY_SAFETY_ENABLED := false
+$(call soong_config_namespace, qti_sdv)
+$(call soong_config_set_bool, qti_sdv, SDV_DISPLAY_SAFETY_ENABLED, false)
+#----------------------------------------------------------------------
 
 #namespace definition for librecovery_updater
 #differentiate legacy 'sg' or 'bsg' framework
