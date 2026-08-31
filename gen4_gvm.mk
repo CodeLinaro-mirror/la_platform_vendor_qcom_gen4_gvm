2008fea3c94ec158aae0ed520297dec571ef1049
Tabassum Tabassum <ttabassu@qti.qualcomm.com>
2026-08-31 11:55:14 +0530

    gen4_gvm: Remove system properties from vendor code
    Remove properties that are not needed from vendor code,
    same properties present in system code.
    
    SELinux permission errors at boot:
    - mmp.enable.3g2, media.aac_51_output_enabled, mm.enable.smoothstreaming:
      Deprecated media properties, not used in current platform.
    - persist.mm.enable.prefetch: Deprecated media prefetch property.
    - persist.timed.enable: Time services property, not applicable on GVM.
    - persist.fuse_sdcard: SD card simulation on /data/media, not needed.
    - ro.qc.sdk.audio.ssr (system.prop only): Duplicate of the entry
    
    Change-Id: I6f91da6bd811bf67ff4a522e1bbefbbf3aaee3a8
    Signed-off-by: Tabassum Tabassum <ttabassu@qti.qualcomm.com>
    (cherry picked from commit 06f25d0c5863555ff3e217004b5fa82306923fde)

--- gen4_gvm.mk
+++ gen4_gvm.mk
@@ -708,21 +708,14 @@ PRODUCT_VENDOR_PROPERTIES += media.stagefright.enable-player=true \
                             media.stagefright.enable-qcp=true \
                             media.stagefright.enable-fma2dp=true \
                             media.stagefright.enable-scan=true \
-                            mmp.enable.3g2=true \
-                            media.aac_51_output_enabled=true \
-                            mm.enable.smoothstreaming=true
 
 #13631487 is decimal sum of supported codecs in AAL
 #codecs:(PARSER_)AAC AC3 AMR_NB AMR_WB ASF AVI DTS FLV 3GP 3G2 MKV MP2PS MP2TS MP3 OGG QCP WAV FLAC AIFF APE DSD MOV XVID
 PRODUCT_VENDOR_PROPERTIES += vendor.mm.enable.qcom_parser=63963135 \
-                            persist.mm.enable.prefetch=true
 
 # system props for the data modules
 PRODUCT_VENDOR_PROPERTIES += ro.vendor.use_data_netmgrd=true \
                             persist.vendor.data.mode=concurrent
-
-#system props for time-services
-PRODUCT_VENDOR_PROPERTIES += persist.timed.enable=true
 
 # system prop for opengles version
 # 196608 is decimal for 0x30000 to report version 3
@@ -732,9 +725,6 @@ PRODUCT_VENDOR_PROPERTIES += ro.opengles.version=196610
 
 # system prop to turn on CdmaLTEPhone always
 PRODUCT_VENDOR_PROPERTIES += telephony.lteOnCdmaDevice=1
-
-#Simulate sdcard on /data/media
-PRODUCT_VENDOR_PROPERTIES += persist.fuse_sdcard=true
 
 PRODUCT_VENDOR_PROPERTIES += persist.vendor.service.bdroid.sibs=false
 
